143e073244b633e6b21856f96484364aed7ead68
memtarhan <hi@memtarhan.com>
2024-11-25 23:52:28 +0300

    [Flask] - first app and home endpoint

---
+++ 8-Developing-AI-Applications-with-Python-and-Flask/TestApp1/server.py
@@ -0,0 +1,10 @@
+# Import the Flask class from the flask module
+from flask import Flask
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def home():
+    return "Hello, World!"
