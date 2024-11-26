5e897ec72daaf0dbf46f727b9154d85c1d1cccc1
memtarhan <hi@memtarhan.com>
2024-11-26 23:12:28 +0300

    [Flask] - dynamic routes

--- 8-Developing-AI-Applications-with-Python-and-Flask/TestApp1/server.py
+++ 8-Developing-AI-Applications-with-Python-and-Flask/TestApp1/server.py
@@ -1,5 +1,6 @@
 # Import the Flask class from the flask module
 from flask import Flask, request, jsonify
+import requests
 
 # Create an instance of the Flask class, passing in the name of the current module
 app = Flask(__name__)
@@ -27,3 +28,27 @@ def get_course_info():
     course = request.args["course"]
     rating = request.args.get("rating")
     return {"message": f"{course} with rating {rating}"}
+
+@app.route("/favorite-author")
+def get_favorite_author():
+    res = requests.get('https://openlibrary.org/authors/OL19981A/Stephen_King')
+    if res.status_code == 200:
+        return {'message': res}
+    elif res.status_code == 404:
+        return {'message': 'Something went wrong!'}, 400
+    else:
+        return {'message': 'Server error'}, 500
+
+
+@app.route("/book/<isbn>")
+def get_author(isbn):
+    res = requests.get("https://openlibrary.org/isbn/{escape(isbn)}.JSON")
+
+    if res.status_code == 200:
+        return {'message': res.JSON()}
+    elif res.status_code == 404:
+        return {'message': 'Something went wrong!'}, 404
+    else:
+        return {'message': 'Server error'}, 500
+
+
