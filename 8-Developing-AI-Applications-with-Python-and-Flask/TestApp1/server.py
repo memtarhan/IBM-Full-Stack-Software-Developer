5553707f916626d0b099efeb5023957d83495258
memtarhan <hi@memtarhan.com>
2024-11-26 13:53:41 +0300

    [Flask] - request and response objects

--- 8-Developing-AI-Applications-with-Python-and-Flask/TestApp1/server.py
+++ 8-Developing-AI-Applications-with-Python-and-Flask/TestApp1/server.py
@@ -1,5 +1,5 @@
 # Import the Flask class from the flask module
-from flask import Flask
+from flask import Flask, request, jsonify
 
 # Create an instance of the Flask class, passing in the name of the current module
 app = Flask(__name__)
@@ -14,3 +14,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {'message': 'Hello, World!'}
+
+@app.route("/health", methods=["GET", "POST"])
+def health():
+    if request.method == "GET":
+        return jsonify(status="OK", method="GET"), 200
+    if request.method == "POST":
+        return jsonify(status="OK", method="POST"), 200
+
+@app.route('/')
+def get_course_info():
+    course = request.args["course"]
+    rating = request.args.get("rating")
+    return {"message": f"{course} with rating {rating}"}
